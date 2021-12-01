449504d6275379d20e75c8c85e2102ed220109b9
ArchDukeInstinct <dukeinstinct@hotmail.com>
2021-11-30 22:51:03 -0800

    Add validator class. Split parsing functions into new file.

---
+++ InfoMarkupParsing.cs
@@ -0,0 +1,288 @@
+﻿using System.Collections.Generic;
+
+namespace InfoMarkup
+{
+    // ========================================================================================================================================
+    // InfoMarkupReader (Parsing)
+    // ========================================================================================================================================
+
+    public partial class InfoMarkupReader
+    {
+        // ====================================================================================================================================
+        // ParseText
+        // ====================================================================================================================================
+
+        private InfoValue ParseText(char delimiter)
+        {
+            Skip(delimiter);
+
+            while (chr != delimiter)
+            {
+                if (chr == '\\')
+                {
+                    // Found a backslash, check if it is escaping a control character or the delimiter
+
+                    Expect();
+
+                    if (chr == '\\')
+                        builder.Append('\\');
+                    else if (chr == 't')
+                        builder.Append('\t');
+                    else if (chr == 'n')
+                        builder.Append('\n');
+                    else if (chr == delimiter)
+                        builder.Append(delimiter);
+                    else
+                    {
+                        builder.Append('\\');
+                        builder.Append(chr);
+                    }
+                }
+                else
+                    builder.Append(chr);
+
+                Expect();
+            }
+
+            SkipUnless('\n');
+
+            return new InfoString(builder.ToString());
+        }
+
+        // ====================================================================================================================================
+        // ParseNumber
+        // ====================================================================================================================================
+
+        private InfoValue ParseNumber()
+        {
+            // Get digits of integer value
+            while (chr >= '0' && chr <= '9')
+            {
+                builder.Append(chr);
+                Expect();
+            }
+
+            // Get remaining digits of a float value if there is a decimal
+            if (chr == '.')
+            {
+                builder.Append(chr);
+
+                while (chr >= '0' && chr <= '9')
+                {
+                    builder.Append(chr);
+                    Expect();
+                }
+            }
+
+            string numStr = builder.ToString();
+            float value = float.Parse(numStr);
+
+            // Attempt to apply optional unit scale or percentage
+            if (chr >= '!' && chr != ']')
+            {
+                builder.Clear();
+                while (chr >= '!' && chr != ']' && chr != ')')
+                {
+                    builder.Append(chr);
+                    Expect();
+                }
+
+                string postfix = builder.ToString();
+
+                if (postfix == "%")
+                    value = GetAttribute(currentKey).GetFloat(0.0f) * (value * 0.01f);
+                else if (!config.ApplyUnitScale(postfix, ref value))
+                    return new InfoString(numStr + postfix);
+            }
+
+            return new InfoNumber(value);
+        }
+
+        // ====================================================================================================================================
+        // ParseModifier
+        // ====================================================================================================================================
+
+        private InfoValue ParseModifier()
+        {
+            AttributeStack attrStack;
+
+            Skip('+');
+
+            if (chr == '-' || chr == '.')
+            {
+                // append the starting minus or decimal
+                builder.Append(chr);
+                Expect();
+            }
+
+            if (!(chr >= '0' && chr <= '9'))
+                return ParseGeneral();
+
+            if (dataMap.TryGetValue(currentKey, out attrStack))
+            {
+                InfoValue val = ParseNumber();
+                InfoNumber num = val as InfoNumber;
+
+                if (num != null)
+                    num.value += (attrStack.values.Count > 0) ? attrStack.values.Peek().GetFloat() : 0.0f;
+
+                return val;
+            }
+
+            // Treat this as a regular number if there is no attribute stack
+            return ParseNumber();
+        }
+
+        // ====================================================================================================================================
+        // ParseGeneral
+        // ====================================================================================================================================
+
+        private InfoValue ParseGeneral()
+        {
+            while (chr >= '!' && chr != ']' && chr != ')')
+            {
+                builder.Append(chr);
+                Expect();
+            }
+
+            return new InfoString(builder.ToString().ToLower());
+        }
+
+        // ====================================================================================================================================
+        // ParseGroup
+        // ====================================================================================================================================
+
+        private InfoValue ParseGroup(InfoValue first, char delimiter, char start = '\0')
+        {
+            List<InfoValue> values = new List<InfoValue>();
+
+            if (first != null)
+                values.Add(first);
+
+            Skip(start);
+
+            while (chr != delimiter)
+            {
+                if (values.Count >= config.groupLimit)
+                    Error("Group maximum count exceeded");
+                if (chr == '\n')
+                    Error("Multiline groups are not currently supported");
+
+                values.Add(ParseToken());
+                NextChr();
+            }
+
+            SkipUnless('\n');
+
+            return new InfoGroup(values.ToArray());
+        }
+
+        private InfoValue ParseGroup(char delimiter, char start = '\0')
+        {
+            return ParseGroup(null, delimiter, start);
+        }
+
+        // ====================================================================================================================================
+        // ParseToken
+        // ====================================================================================================================================
+
+        private InfoValue ParseToken()
+        {
+            builder.Clear();
+
+            if (chr == '\'')
+                return ParseText('\'');
+            else if (chr == '"')
+                return ParseText('"');
+            else if (chr == '(')
+                return ParseGroup(')', '(');
+            else
+            {
+                if (chr == '+')
+                    return ParseModifier();
+                else if (chr == '-' || chr == '.')
+                {
+                    // append the starting minus or decimal
+                    builder.Append(chr);
+                    Expect();
+
+                    if ((chr >= '0' && chr <= '9'))
+                        return ParseNumber();
+                    else
+                        return ParseGeneral();
+                }
+                else if ((chr >= '0' && chr <= '9'))
+                    return ParseNumber();
+                else
+                    return ParseGeneral();
+            }
+        }
+
+        // ====================================================================================================================================
+        // ParseElement
+        // ====================================================================================================================================
+
+        private void ParseElement()
+        {
+            Skip('[');
+            NextChr();
+
+            // Create the scope for this element
+            ScopePush(depthCurr);
+
+            // Get element tag
+            builder.Clear();
+            while (chr >= '!')
+            {
+                if (chr == ']')
+                    break;
+
+                builder.Append(chr);
+                Expect();
+            }
+
+            scope.tag = builder.ToString().ToLower();
+            currentTag = scope.tag;
+
+            // Get optional element parameters
+            NextChr();
+            parameters = (chr != ']') ? ParseGroup(']') : new InfoValue();
+            scope.parameters = parameters;
+
+            currentNodeType = NodeType.ElementStart;
+        }
+
+        // ====================================================================================================================================
+        // ParseAttribute
+        // ====================================================================================================================================
+
+        private void ParseAttribute()
+        {
+            builder.Clear();
+
+            currentValue = ParseToken();
+            NextChr();
+
+            if (chr != '\n')
+            {
+                currentNodeType = NodeType.Attribute;
+                currentKey = currentValue.GetString().ToLower();
+
+                builder.Clear();
+                currentValue = ParseToken();
+
+                NextChr();
+
+                if (chr >= '!')
+                    currentValue = ParseGroup(currentValue, '\n');
+
+                AddAttribute(currentKey, currentValue);
+            }
+            else
+            {
+                currentNodeType = NodeType.Option;
+                currentKey = null;
+            }
+        }
+    }
+}
---
+++ InfoMarkupValidator.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InfoMarkup
+{
+    // ========================================================================================================================================
+    // InfoMarkupValidator
+    // ========================================================================================================================================
+
+    public class InfoMarkupValidator
+    {
+        // ====================================================================================================================================
+        // Data
+        // ====================================================================================================================================
+
+        private class ParameterRules
+        {
+            public bool allowNumber;
+            public bool allowString;
+            public bool allowGroup;
+            public bool isOptional;
+        }
+
+        private class ElementRules
+        {
+            public ParameterRules[] allowedParameters;
+            public int minParameters;
+            public HashSet<string> allowedContainers;
+            public HashSet<string> allowedAttributes;
+            public bool optionsContainer;
+
+            public ElementRules()
+            {
+                allowedParameters   = null;
+                minParameters       = 0;
+                allowedContainers   = null;
+                allowedAttributes   = null;
+            }
+        }
+
+        private Dictionary<string, ElementRules> elementRules;
+
+        // ====================================================================================================================================
+        // Constructor
+        // ====================================================================================================================================
+
+        public InfoMarkupValidator()
+        {
+            elementRules = new Dictionary<string, ElementRules>();
+        }
+
+        // ====================================================================================================================================
+        // SetElementRules
+        // ====================================================================================================================================
+
+        public void SetElementRules(string tag, string allowedParameters, string allowedContainers, string allowedAttributes)
+        {
+            ElementRules rules;
+            HashSet<string> set;
+            string[] tokens, subtokens;
+            int i;
+
+            if (!elementRules.TryGetValue(tag, out rules))
+            {
+                rules = new ElementRules();
+                elementRules.Add(tag, rules);
+            }
+
+            // Add allowed parameters
+            tokens                  = allowedParameters.Split(',');
+            rules.allowedParameters = new ParameterRules[tokens.Length];
+            i                       = 0;
+
+            foreach (string token in tokens)
+            {
+                string p = token.ToLower();
+
+                rules.allowedParameters[i] = new ParameterRules();
+
+                if (p.EndsWith("?"))
+                {
+                    p = p.Substring(0, p.Length - 1);
+                    rules.allowedParameters[i].isOptional = true;
+                }
+
+                subtokens = p.Split('|');
+
+                foreach (string t in subtokens)
+                {
+                    switch (t)
+                    {
+                        case "number":
+                        case "int":
+                        case "float":
+                            rules.allowedParameters[i].allowNumber = true;
+                            break;
+
+                        case "string":
+                        case "text":
+                            rules.allowedParameters[i].allowString = true;
+                            break;
+
+                        case "group":
+                        case "array":
+                            rules.allowedParameters[i].allowGroup = true;
+                            break;
+                    }
+                }
+
+                ++i;
+            }
+
+            // Add allowed containers
+            set     = new HashSet<string>();
+            tokens  = allowedContainers.Split(',');
+            foreach (string token in tokens)
+                set.Add(token.Trim());
+
+            rules.allowedContainers = set;
+
+            // Add allowed attributes
+            set     = new HashSet<string>();
+            tokens  = allowedAttributes.Split(',');
+            foreach (string token in tokens)
+                set.Add(token.Trim());
+
+            rules.allowedAttributes = set;
+        }
+
+        // ====================================================================================================================================
+        // Validate
+        // ====================================================================================================================================
+
+        public bool Validate(InfoMarkupReader reader, ref string errorMsg)
+        {
+            ElementRules element;
+
+            if (reader.scope == null)
+                return true;
+
+            switch (reader.currentNodeType)
+            {
+                case NodeType.ElementStart:
+                    if (!elementRules.TryGetValue(reader.currentTag, out element))
+                    {
+                        errorMsg = string.Format("Element '{0}' does not exist in ruleset.", reader.currentTag);
+                        return false;
+                    }
+
+                    if (reader.scope.parent != null)
+                    {
+                        if (!element.allowedContainers.Contains(reader.scope.parent.tag))
+                        {
+                            errorMsg = string.Format("Element '{0}' cannot be within element '{1}'.", reader.currentTag, reader.scope.parent.tag);
+                            return false;
+                        }
+                    }
+                    else if (!element.allowedContainers.Contains("/"))
+                    {
+                        errorMsg = string.Format("Element '{0}' cannot be a top level element.", reader.currentTag);
+                        return false;
+                    }
+
+                    InfoValue parameters    = reader.scope.parameters;
+                    int pCount              = parameters.GetCount();
+
+                    if (pCount < element.minParameters || pCount > element.allowedParameters.Length)
+                    {
+                        errorMsg = string.Format("Element '{0}' should have {1} to {2} parameters, but was given {3}", reader.currentTag, element.minParameters, element.allowedParameters.Length, pCount);
+                        return false;
+                    }
+
+                    for (int p = 0; p < pCount; ++p)
+                    {
+                        switch (parameters.GetSubValue(p).GetValueType())
+                        {
+                            case ValueType.Number:
+                                if (!element.allowedParameters[p].allowNumber)
+                                {
+                                    errorMsg = string.Format("Parameter type mismatch for Element '{0}'", reader.currentTag);
+                                    return false;
+                                }
+                                break;
+
+                            case ValueType.Text:
+                                if (!element.allowedParameters[p].allowString)
+                                {
+                                    errorMsg = string.Format("Parameter type mismatch for Element '{0}'", reader.currentTag);
+                                    return false;
+                                }
+                                break;
+
+                            case ValueType.Group:
+                                if (!element.allowedParameters[p].allowGroup)
+                                {
+                                    errorMsg = string.Format("Parameter type mismatch for Element '{0}'", reader.currentTag);
+                                    return false;
+                                }
+                                break;
+                        }
+                    }
+                    break;
+
+                case NodeType.Attribute:
+                    if (elementRules.TryGetValue(reader.currentTag, out element))
+                    {
+                        if (!element.allowedAttributes.Contains(reader.currentKey))
+                        {
+                            errorMsg = string.Format("Attribute '{0}' is not allowed in element '{1}'", reader.currentKey, reader.currentTag);
+                            return false;
+                        }
+                    }
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
